4252d700d69de61bee9bed97f09f62361db0e229
devCaiqueDePaula <dev.caiquedepaula@gmail.com>
2024-07-12 22:24:30 -0300

    Create Project

---
+++ UrlShortener/src/main/java/dev/url/UrlShortener/Links/Link.java
@@ -0,0 +1,4 @@
+package dev.url.UrlShortener.Links;
+
+public class Link {
+}
---
+++ UrlShortener/src/test/java/dev/url/UrlShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.url.UrlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
